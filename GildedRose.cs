e2ba49a264dea43914bd133255bf856c9e37ce2b
Edvinas Budzila <edvinas.budzila@gmail.com>
2021-02-18 23:05:07 +0200

    Add project files.

---
+++ GildedRose.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+        public void UpdateQuality()
+        {
+            foreach (Item item in Items)
+            {
+                switch (item.Name)
+                {
+                    case "Aged Brie":
+                        item.UpdateItemValues(ItemType.BetterWithAging);
+                        break;
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        item.UpdateItemValues(ItemType.BackstagePass);
+                        break;
+                    case "Sulfuras, Hand of Ragnaros":
+                        item.UpdateItemValues(ItemType.Legendary);
+                        break;
+                    case "Conjured Mana Cake":
+                        item.UpdateItemValues(ItemType.Conjured);
+                        break;
+                    default:
+                        item.UpdateItemValues(ItemType.Default);
+                        break;
+                }
+            }
+        }
+    }
+
+    public static class GildedRoseExtensions
+    {
+        public static void UpdateItemValues(this Item item, ItemType itemType)
+        {
+            if (itemType != ItemType.Legendary)
+                item.SellIn--;
+
+            int qualityDecrease = item.SellIn >= 0 ? 1 : 2;
+
+            if (itemType == ItemType.Default)
+                item.Quality -= qualityDecrease;
+            else if (itemType == ItemType.Conjured)
+                item.Quality -= qualityDecrease * 2;
+            else if (itemType == ItemType.BetterWithAging)
+                item.Quality++;
+            else if (itemType == ItemType.BackstagePass)
+            {
+                item.Quality++;
+                if (item.SellIn < 10)
+                    item.Quality++;
+                if (item.SellIn < 5)
+                    item.Quality++;
+                if (item.SellIn < 0)
+                    item.Quality = 0;
+            }
+
+            if (item.Quality < 0)
+                item.Quality = 0;
+            else if (item.Quality > 50 && itemType != ItemType.Legendary)
+                item.Quality = 50;
+        }
+    }
+    public enum ItemType
+    {
+        Legendary,
+        BackstagePass,
+        Conjured,
+        BetterWithAging,
+        Default
+    }
+}
---
+++ GildedRoseTest.cs
@@ -0,0 +1,147 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        [Test]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("foo", Items[0].Name);
+        }
+        [Test]
+        public void TestUsualItem()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "Something simple", SellIn = 3, Quality = 6 } };
+            GildedRose app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.AreEqual(2, items[0].SellIn);
+            Assert.AreEqual(5, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(1, items[0].SellIn);
+            Assert.AreEqual(4, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(0, items[0].SellIn);
+            Assert.AreEqual(3, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(-1, items[0].SellIn);
+            Assert.AreEqual(1, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(-2, items[0].SellIn);
+            Assert.AreEqual(0, items[0].Quality);
+        }
+        [Test]
+        public void TestAgedBrie()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 3, Quality = 47 } };
+            GildedRose app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.AreEqual(2, items[0].SellIn);
+            Assert.AreEqual(48, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(1, items[0].SellIn);
+            Assert.AreEqual(49, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(0, items[0].SellIn);
+            Assert.AreEqual(50, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(-1, items[0].SellIn);
+            Assert.AreEqual(50, items[0].Quality);
+        }
+        [Test]
+        public void TestBackstagePass()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = 10 } };
+            GildedRose app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.AreEqual(10, items[0].SellIn);
+            Assert.AreEqual(11, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(9, items[0].SellIn);
+            Assert.AreEqual(13, items[0].Quality);
+
+            app.UpdateQuality();
+            app.UpdateQuality();
+            app.UpdateQuality();
+            app.UpdateQuality();
+
+            Assert.AreEqual(5, items[0].SellIn);
+            Assert.AreEqual(21, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(4, items[0].SellIn);
+            Assert.AreEqual(24, items[0].Quality);
+
+            app.UpdateQuality();
+            app.UpdateQuality();
+            app.UpdateQuality();
+            app.UpdateQuality();
+            app.UpdateQuality();
+
+            Assert.AreEqual(-1, items[0].SellIn);
+            Assert.AreEqual(0, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(-2, items[0].SellIn);
+            Assert.AreEqual(0, items[0].Quality);
+        }
+
+        [Test]
+        public void TestSulfuras()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 3, Quality = 80 } };
+            GildedRose app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.AreEqual(3, items[0].SellIn);
+            Assert.AreEqual(80, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(3, items[0].SellIn);
+            Assert.AreEqual(80, items[0].Quality);
+        }
+
+        [Test]
+        public void TestConjured()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 10 } };
+            GildedRose app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.AreEqual(2, items[0].SellIn);
+            Assert.AreEqual(8, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(1, items[0].SellIn);
+            Assert.AreEqual(6, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(0, items[0].SellIn);
+            Assert.AreEqual(4, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(-1, items[0].SellIn);
+            Assert.AreEqual(0, items[0].Quality);
+
+            app.UpdateQuality();
+            Assert.AreEqual(-2, items[0].SellIn);
+            Assert.AreEqual(0, items[0].Quality);
+        }
+    }
+}
